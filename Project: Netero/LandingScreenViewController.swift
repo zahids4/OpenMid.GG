0cf178930af67eeda644c5764a7adadeda45089d
Saim Zahid <saimzahid@me.com>
2018-05-31 18:25:00 -0400

    Making the initial launch and lashing screens.

---
+++ Project: Netero/LandingScreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LandingScreenViewController.swift
+//  Project: Netero
+//
+//  Created by Saim Zahid on 2018-05-31.
+//  Copyright © 2018 Saim Zahid. All rights reserved.
+//
+
+import UIKit
+
+class LandingScreenViewController: UIViewController {
+    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchButton.layer.cornerRadius = 5.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
